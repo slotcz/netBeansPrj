8398e1bdc3a232e6544b9c75f3eda9bc0ab4c368
slotcz <33904397+slotcz@users.noreply.github.com>
2017-11-22 16:56:57 +0100

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/MainTestClientSTB.java
@@ -0,0 +1,23 @@
+package com.mycompany.mavenproject1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author slotcz
+ */
+public class MainTestClientSTB {
+
+    public MainTestClientSTB() {
+    }
+    
+    public void main(){
+        MainTestClientSTB mtc = new MainTestClientSTB();
+        System.out.println(mtc.toString());
+    }
+}
